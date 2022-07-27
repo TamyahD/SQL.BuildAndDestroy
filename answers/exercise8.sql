efb0ca0cac07b883d65ecdc54313af2f00fa661e
Myah <myah@Zipcoders-MacBook-Pro-61.local>
2022-07-27 09:56:07 -0400

    10 down, more to go....

---
+++ answers/exercise8.sql
@@ -0,0 +1,7 @@
+Command #1:
+CREATE TABLE Students (StudentName VARCHAR(50), Address VARCHAR(255), City 
+VARCHAR(50), PostalCode VARCHAR(5), Country VARCHAR(50));
+
+Command #2:
+INSERT INTO Students (StudentName, Address, City, PostalCode, Country) VALUES 
+('Jane Does', '57 Union St', 'Glasgow', 'G13RB', 'Scotland');
